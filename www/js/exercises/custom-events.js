f4b95aee7d30e24cf2fcd34c5326092ef24850d4
Rebecca Murphey <rebecca@rebeccamurphey.com>
2010-12-15 23:36:08 -0500

    updating custom events task

--- www/js/exercises/custom-events.js
+++ www/js/exercises/custom-events.js
@@ -1,21 +1,14 @@
+$(function() {
+
 /**
- * TASK: Use custom events and a mediator to communicate between the 
- * #filters container and the #cast container to determine which cast
- * members are shown. 
+ * TASK: Use custom events to make it so that when a checkbox is clicked
+ * in the #filters container, the list items in the #cast container are 
+ * shown or hidden depending on whether they have a class that matches
+ * the selected checkboxes.
+ *
+ * Then, add another container to the page that listens for an event to
+ * be broadcast by the #cast container when the items it is showing change,
+ * and lists the data-name attributes of the showing items. 
  */
 
-$(function() {
-  var filters = $('#filters'), 
-      cast = $('#cast'),
-      doc = $(document);
-
-  filters.find('input').bind('click', function() {
-    doc.trigger('filterSelection', [ $(this).val(), this.checked ]);
-  });
-
-  doc.bind('filterSelection', function(e, val, checked) {
-    cast.find('li.' + val)[checked ? 'show' : 'hide'](); 
-  });
-
-  cast.find('li').hide();
 });
